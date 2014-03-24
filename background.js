6c7b09665d1d5d99acd70d15b39545a7c888dda6
Alex <alex.hyrenko@gmail.com>
2014-03-24 14:34:19 +0200

    resizable

--- background.js
+++ background.js
@@ -1,7 +1,6 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
 	id: 'parent',
-	"resizable": false,
 	'bounds': {
 		'width'		: 1200,
 		'height'	: 950
